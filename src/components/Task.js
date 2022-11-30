08d1f843920a61070b6d674539118e110bdb488c
Wes Reimer <474540+MrFussyfont@users.noreply.github.com>
2022-11-30 17:30:29 -0500

    changed bg colour

--- src/components/Task.js
+++ src/components/Task.js
@@ -15,7 +15,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           name="checked"
           id={`archiveTask-${id}`}
           checked={state === "TASK_ARCHIVED"}
-          style={{ background: 'red' }}
+          
         />
         <span
           className="checkbox-custom"
@@ -30,6 +30,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           readOnly={true}
           name="title"
           placeholder="Input title"
+          style={{ background: 'red' }}
         />
       </label>
 
